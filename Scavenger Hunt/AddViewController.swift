4e7616a1b9bbafcf348cf3672f7cdb3f2bb16d5d
Apple <apple@Apples-MacBook-7.local>
2016-03-26 16:50:06 -0400

    first commit

--- Scavenger Hunt/AddViewController.swift
+++ Scavenger Hunt/AddViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AddViewController.swift
 //  Scavenger Hunt
 //
 //  Created by Apple on 3/26/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AddViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Scavenger Hunt/ListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Apple on 3/26/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import Foundation
